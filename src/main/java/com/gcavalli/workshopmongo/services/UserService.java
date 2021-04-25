e2cc25cef2f49267c5432609aaaecdf27672e5fb
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-04-25 15:05:05 -0300

    Insercao de User com POST

--- src/main/java/com/gcavalli/workshopmongo/services/UserService.java
+++ src/main/java/com/gcavalli/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gcavalli.workshopmongo.domain.User;
+import com.gcavalli.workshopmongo.dto.UserDTO;
 import com.gcavalli.workshopmongo.repositories.UserRepository;
 import com.gcavalli.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado. Id: "+id+"."));
+	}
+	
+	public User insert(User obj) {
+		return repo.save(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
