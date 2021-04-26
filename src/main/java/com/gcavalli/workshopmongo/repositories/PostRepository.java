d43e0a08d934cf084bb1b9a04adf94ada926f5e5
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-04-25 22:14:42 -0300

    Consulta simples com @Query

--- src/main/java/com/gcavalli/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/gcavalli/workshopmongo/repositories/PostRepository.java
@@ -3,10 +3,15 @@ package com.gcavalli.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.gcavalli.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	//Exemplo utilizando a anotacao Query
+	@Query("{ 'title': { $regex: ?0, $options: 'i'} }")
+	List<Post> seachByTitle(String text);
 	
 	public List<Post> findByTitleContainingIgnoreCase(String text);
 }
